cfaa49685902b61845d305523f00d9ab8d37c745
Jason <jhedlund79@gmail.com>
2014-03-23 20:32:50 -0700

    Just removed commented code and fixed texture referenced before assignment error I was getting.

--- UI_Module.py
+++ UI_Module.py
@@ -72,9 +72,11 @@ class UI_Driver(bpy.types.Operator, ImportHelper):
         input_DEM = bpy.path.ensure_ext(input_DEM, '.IMG')
         dtm_location = self.filepath
         project_location = os.path.dirname(__file__)
+        #Declare the variables we will be using later in this function
         hill_shade = ''
         color_relief = ''
         color_file = ''
+        texture_location = ''
         print("Here it is: ",project_location)
 
         ################################################################################
@@ -85,33 +87,6 @@ class UI_Driver(bpy.types.Operator, ImportHelper):
         if self.color_pattern == 'NoColorPattern':
             pass
         else:
-            # # If user selected a colr we are going to run the gdal and merge processes
-            # # We need to dtermine which OS is being used and set the location of color files
-            # # and the merge script accordingly
-            # if _platform == "linux" or _platform == "linux2":
-            # # linux
-            #         # Strip out the image name to set texture location and append color choice.
-            #     texture_location = self.filepath.split('/')[-1:]
-            #     texture_location = texture_location[0].split('.')[:1]
-            #     texture_location = os.getcwd()+'/'+texture_location[0]+'_'+self.color_pattern+'.tiff'
-            #     color_file = '/usr/share/blender/scripts/addons/USGS/color_maps/' + self.color_pattern + '.txt'
-            #     merge_location = '/usr/share/blender/scripts/addons/USGS/hsv_merge.py'
-            #     hill_shade = os.path.normpath("\""+project_location+"/maps/hillshade.tiff\"")
-            #     color_relief = os.path.normpath("\""+project_location+"/maps/colorrelief.tiff\"")
-            # elif _platform.system() == "darwin":
-            # # OS X
-            #             # Strip out the image name to set texture location and append color choice.
-            #     texture_location = self.filepath.split('/')[-1:]
-            #     texture_location = texture_location[0].split('.')[:1]
-            #     texture_location = os.getcwd()+'/'+texture_location[0]+'_'+self.color_pattern+'.tiff'
-            #     color_file = '/Applications/Blender/blender.app/Contents/MacOS/2.69/scripts/addons/USGS/color_maps/'\
-            #         + self.color_pattern + '.txt'
-            #     merge_location = '/Applications/Blender/blender.app/Contents/MacOS/2.69/scripts/addons/USGS/hsv_merge.py'
-            #     hill_shade = os.path.normpath("\""+project_location+"/maps/hillshade.tiff\"")
-            #     color_relief = os.path.normpath("\""+project_location+"/maps/colorrelief.tiff\"")
-            # elif _platform.system() == "Windows":
-            # Windows.
-            #consider using this for all systems should be cross compatible... but I can't test it outside of windows yet
             color_file = os.path.normpath("\"" + project_location + "/color_maps/" + self.color_pattern + ".txt\"")
             merge_location = os.path.normpath("\"" + project_location + "/hsv_merge.py\"")
             texture_location = os.path.normpath(dtm_location + ".tiff")
