60384cf5baf7439a6bbaab9cdbe71e5d603a9093
Terence Luther <tl377@nau.edu>
2014-03-24 23:09:56 -0700

    Done getting a liner fly path set up. Major clean up has been done for readability. Comments have been added to all the functions for easy to read code.
    Work Done:
    >>>Can now get a liner path over an image. Goes from one midpoint to the other, based on which path would be the longest.
    >>>Make path can now add N number of elements and be accurate.  Before it would shift, this was fixed.
    >>>Further work to be done will be to get this put into the FLYOVER module.
    >>>We will also need to place a camera on the path and provide parameters to how many frames will be accepted.
    >>>We can accomplish this by following the same logic as the circular fly-over.

--- test.py
+++ test.py
@@ -2,76 +2,97 @@ import bpy
 import math
 
 
+#Simple test to ensure that import is good.
+#Will be gotten rid when implementing functions into flyover.
 def test_func():
     print("Import good!")
 
 
+#Helper function to get the distance between two functions.
+def distance_two_points(point_one, point_two):
+    distance = math.sqrt((point_one[0] - point_two[0])*(point_one[0] - point_two[0]) + (point_one[1] - point_two[1])*(point_one[1] - point_two[1]) + (point_one[2] - point_two[2])*(point_one[2] - point_two[2]))
+    return distance
+
+
+#Helper function to find the midpoint between two points.
+def midpoint_two_points(point_one, point_two):
+    return (point_one[0]+point_two[0])/2, (point_one[1]+point_two[1])/2, (point_one[2]+point_two[2])/2
+
+#Function to get a simple liner path for the overall DEM MESH.
+#Gets the path by calculating the midpoints in the DEM image and makes the path run through the long ways of the DEM.
 def get_liner_path():
-    temp = None
-    line_counter = 0
-    line_length = 0
-    holder = 0
-    temp_count = 0
-    vertex_list = []
+    #Simple value holders for getting our corners and highest point in the DEM.
+    #Farthest NW corner of the DEM.
+    x_max_point = (0, 0, 0)
+    #Farthest SE corner of the DEM.
+    x_min_point = (100, 0, 0)
+    #Farthest NE corner of the DEM.
+    y_max_point = (0, 0, 0)
+    #Farthest SW corner of the DEM.
+    y_min_point = (0, 100, 0)
+    #Max height value of the DEM.
+    z_max_value = -10
+    #Run through each object to find the MESH.
     for item in bpy.data.objects:
-        #print(item.name)
         if item.type == 'MESH':
+            #Run through each vertex to get our data.
             for vertex in item.data.vertices:
-                if temp is None:
-                    pass
-                elif line_length > 0:
-                    if line_length/2 == line_counter % line_length:
-                        #print(vertex.co)
-                        #print(line_length/2)
-                        #print(line_counter)
-                        vertex_list.append(vertex.co)
-                else:
-                    distance = math.sqrt((temp.x - vertex.co.x)*(temp.x - vertex.co.x) + (temp.y - vertex.co.y)*(temp.y - vertex.co.y) + (temp.z - vertex.co.z)*(temp.z - vertex.co.z))
-                    if distance > holder:
-                        holder = distance
-                    if distance > 1:
-                        temp_count += 1
-                        line_length = line_counter
-                    #first = vertex.co.y
-                    #if first < 0:
-                    #    first = -1 * first
-                    #second = temp.y
-                    #if second < 0:
-                    #    second = -1* second
-                    #check = first - second
-                    #if holder < check:
-                    #    holder = check
-                    #if check > 1:
-                    #    line_length = line_counter
-                    #old stuff.
-                line_counter += 1
-                temp = vertex.co
-    output_list = []
-    print(line_counter)
-    print(line_length)
-    print(holder)
-    print(temp_count)
-    for vertex in vertex_list:
-        point = (vertex.x, vertex.y, vertex.z + .5)
-        output_list.append(point)
-    return output_list
+                #Series of if statements to get the correct values for our value holders.
+                if vertex.co.x >= x_max_point[0]:
+                    x_max_point = (vertex.co.x, vertex.co.y, vertex.co.z)
+                if vertex.co.x < x_min_point[0]:
+                    x_min_point = (vertex.co.x, vertex.co.y, vertex.co.z)
+                if vertex.co.y >= y_max_point[1]:
+                    y_max_point = (vertex.co.x, vertex.co.y, vertex.co.z)
+                if vertex.co.y < y_min_point[1]:
+                    y_min_point = (vertex.co.x, vertex.co.y, vertex.co.z)
+                if vertex.co.z > z_max_value:
+                    z_max_value = vertex.co.z
+    #List holders for work to be done.
+    #Holds the midpoints we are going to work with.
+    midpoint_holder = []
+    #Our return list of the values to be returned from this function.
+    return_list = []
+    #Getting a new height value to be used for the path.
+    #May change to a parameter later to be dynamic.
+    z_max_value += 0.5
+    #If statement to see which way is longer.
+    #Helps us find out which way the path will run.
+    if distance_two_points(x_min_point, y_max_point) > distance_two_points(x_min_point, y_min_point):
+        midpoint_holder.append(midpoint_two_points(x_min_point, y_min_point))
+        midpoint_holder.append(midpoint_two_points(x_max_point, y_max_point))
+    else:
+        midpoint_holder.append(midpoint_two_points(x_min_point, y_max_point))
+        midpoint_holder.append(midpoint_two_points(x_max_point, y_min_point))
+    #Loop to give us our new points to be returned.
+    for point in midpoint_holder:
+        new_point = (point[0], point[1], z_max_value)
+        return_list.append(new_point)
+    #Final return of get liner path function.
+    return return_list
 
 
+#Creates a poly path out of N points.
 def make_path(object_name, curve_name, points):
+    #Sets up or curve and object to be added to the scene.
     curve_data = bpy.data.curves.new(name=curve_name, type='CURVE')
     curve_data.dimensions = '3D'
     object_data = bpy.data.objects.new(object_name, curve_data)
-    #starting point
+    #Starting point of our curve. The first point in our input list.
     object_data.location = points[0]
     bpy.context.scene.objects.link(object_data)
-    #smooth curve
+    #Type of curve, POLY, and the number of points to be added.
     polyline = curve_data.splines.new('POLY')
     polyline.points.add(len(points)-1)
+    #Need a holder for our origin.
+    o_x, o_y, o_z = (0, 0, 0)
     for index in range(len(points)):
         if(index == 0):
-            pass
+            #First iteration gives or holder the value of the curve origin.
+            o_x, o_y, o_z = points[index]
         else:
+            #Because the origin of the curve is different from (0, 0, 0),
+            #we need to change the following points relative to our curve origin.
+            #As if our curve origin is (0, 0, 0).
             x, y, z = points[index]
-            polyline.points[index].co = (x, y, z, 1)
-            #polyline.order_u = len(polyline.points) - 1
-            #polyline.use_endpoint_u = True
+            polyline.points[index].co = ((x - o_x), (y - o_y), z, 1)
