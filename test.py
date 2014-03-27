04e5053cd23e624a45729ed8be81cbef654bed03
Terence Luther <tl377@nau.edu>
2014-03-26 21:09:29 -0700

    Working on camera in test file.

--- test.py
+++ test.py
@@ -2,3 +2,13 @@
 ###Current functions ported over to their respective modules.####
 ###File in place for future function development.################
 #################################################################
+import bpy
+
+
+def make_camera(point, target_point):
+    camera_object = B.Object.New('Camera')
+    camera_data = B.Camera.New('view')
+    camera_object.link(camera_data)
+    scene.link(camera_object)
+    camera_object.loc = point
+    return
