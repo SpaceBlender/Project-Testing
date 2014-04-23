efbe673cbd5f0acb08854e385918d63ba43f4473
Andrew Carter <ajc388@nau.edu>
2014-04-22 21:55:45 -0700

    This is the completion of the gdal unit tests. Once, I make my first pass through modules, I may add more.
    Revision Includes:
    1.) Unit tests for hsv_merge.
    2.) Decision to not test gdal cleanup, since it has already been run over 44 times and other tests would fail if it wasn't working.

--- test_gdal.py
+++ test_gdal.py
@@ -1,19 +1,21 @@
 __author__ = "Andrew"
-
 from . import gdal_module
 import unittest
 import os
-
-#The DEM you want to use for gdal unit tests
-#Use smaller DEM's if you want this to actually finish
+######################################################################################
+#                      The DEMs for the following tests                              #
+######################################################################################
 dem1 = "C:\\Users\\Andrew\\Desktop\\DEMs\\DTEED_020492_1830_021481_1830_A01.IMG"
 dem2 = "C:\\Users\\Andrew\\Desktop\\DEMs\\DTEEC_017569_1645_016857_1645_A01.IMG"
-dem3 = ""
 
-#Helpful variables for all tests
+######################################################################################
+#                   Helpful variables used in all of the tests                       #
+######################################################################################
 project_location = os.path.dirname(__file__)
 color_destination = os.path.normpath("\""+project_location+"/maps/colorrelief.tiff\"")
 hillshade_destination = os.path.normpath("\""+project_location+"/maps/hillshade.tiff\"")
+hsv_merge_location = os.path.normpath("\""+project_location+"/hsv_merge.py\"")
+texture_location = os.path.normpath("\""+project_location+"/maps/texture.tiff\"")
 
 # Assumptions
 #1.) The gdal and hsv_merge utiltiies are assumed to be pre-tested for correctness
@@ -38,10 +40,10 @@ class TestGdalHillshade(unittest.TestCase):
         self.assertTrue(return_value)
         gdal.gdal_clean_up(hillshade_destination, "")
 
-#All of these tests do not test for correctness of GDAL
-#These tests check for correctness of our created color files by processing them through gdal
-#If they process, the color file was formed correctly... there's better ways to do this
-#But i'm lazy and this fufills the concept logically
+# #All of these tests do not test for correctness of GDAL
+# #These tests check for correctness of our created color files by processing them through gdal
+# #If they process, the color file was formed correctly... there's better ways to do this
+# #But i'm lazy and this fufills the concept logically
 class TestGdalColorrelief(unittest.TestCase):
     def test_gdal_color_relief_blue_steel(self):
         gdal = gdal_module.GDALDriver(dem1)
@@ -188,13 +190,37 @@ class TestGdalColorrelief(unittest.TestCase):
         gdal.gdal_clean_up(color_destination, "")
 
 
+#This set of tests could have a lot more, the problem is it takes a very long time to process
+#to make sure it is working correctly, it needs to generate color reliefs and hill shades
+#and then merge them together.
 class TestGdalHsvMerge(unittest.TestCase):
-    def test_hsv_merge(self):
-        pass
+    def test_hsv_merge_dem1_sequential_red(self):
+        gdal = gdal_module.GDALDriver(dem1)
 
+        gdal.gdal_clean_up(color_destination, hillshade_destination)
+        color_relief = os.path.normpath("\""+project_location+"/color_maps/Sequential_Red.txt\"")
+        gdal.gdal_color_relief(color_relief, color_destination)
+        gdal.gdal_hillshade(hillshade_destination)
 
-class TestGdalCleanup(unittest.TestCase):
-    def test_gdal_cleanup(self):
-        pass
+        return_value = gdal.hsv_merge(hsv_merge_location, hillshade_destination, color_destination, texture_location)
+        self.assertTrue(return_value)
+        gdal.gdal_clean_up(color_destination, hillshade_destination)
+        gdal.gdal_clean_up(texture_location, '')
+
+    def test_hsv_merge_dem2_sequential_red(self):
+        gdal = gdal_module.GDALDriver(dem2)
+
+        gdal.gdal_clean_up(color_destination, hillshade_destination)
+        color_relief = os.path.normpath("\""+project_location+"/color_maps/Sequential_Green.txt\"")
+        gdal.gdal_color_relief(color_relief, color_destination)
+        gdal.gdal_hillshade(hillshade_destination)
+
+        return_value = gdal.hsv_merge(hsv_merge_location, hillshade_destination, color_destination, texture_location)
+        self.assertTrue(return_value)
+        gdal.gdal_clean_up(color_destination, hillshade_destination)
+        gdal.gdal_clean_up(texture_location, '')
+
+#At this point gdal cleanup has been run over 44 times no tests are needed for gdal cleanup to verify it's functionality
+
 
 
